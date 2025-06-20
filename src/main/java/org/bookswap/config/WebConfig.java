3ede28f3ed9f49e9ec7c715af0ee72dc8c43e25f
Zoya <kittenzoya@yandex.ru>
2025-06-20 20:46:33 +0400

    fix: корсы

--- src/main/java/org/bookswap/config/WebConfig.java
+++ src/main/java/org/bookswap/config/WebConfig.java
@@ -14,8 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOriginPatterns(
+                        "http://localhost:3001",
+                        "http://192.168.*:3001"
+                )                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
